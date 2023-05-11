68458d9d7c8c83f42637c3babee796d2de6cbb3f
searce-thejaswini <107244346+searce-thejaswini@users.noreply.github.com>
2023-05-11 11:31:14 +0530

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -16,3 +16,19 @@ data "google_compute_subnetwork" "my_subnetwork" {
     google_compute_subnetwork.gke-custom-snet
   ]
 }
+
+data "google_container_cluster" "asm" {
+  project  = var.project_id
+  #name     = var.cluster_mode == "private" ? "${var.clustername}-${var.environment}-gke-pvt-cluster-01" : "${var.clustername}-${var.environment}-gke-pub-cluster-011"
+  name     = "${var.clustername}-${var.environment}-gke-pub-cluster-011"
+  # location = var.cluster_type == "regional" ? var.region : "${var.region}-a"
+  location   = var.region
+
+  # depends_on = [google_container_cluster.gke_standard_private, google_container_cluster.gke_standard_public]
+  depends_on = [ google_container_cluster.gke_standard_public ]
+
+}
+
+data "google_client_config" "default" {}
+
+
